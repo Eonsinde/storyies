0243f63edd70174fae1c61721d2a2d534a188aad
eonsinde <olasinde.eon@gmail.com>
2022-11-04 18:01:41 +0100

    included passport and auth routes

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
+const passport = require('passport');
+const session = require('express-session');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const connectDB = require('./config/db');
 
 // load config
 dotenv.config({ path: './config/config.env' });
+
+// passport config
+require('./config/passport')(passport)
 
 connectDB();
 
@@ -20,11 +25,25 @@ if (process.env.NODE_ENV === 'development') {
 app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
+// sessions: put above passport middleware
+app.use(session({
+    secret: 'shazam',
+    resave: false,
+    saveUninitialized: false,
+    // cookie: { secure: true },
+    // store: ''
+}))
+
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 5000;
 
